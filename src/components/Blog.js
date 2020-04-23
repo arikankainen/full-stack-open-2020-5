2cee17dab5d848c87f5c87b3cc7c3e4e9060dc4b
arikankainen <60932647+arikankainen@users.noreply.github.com>
2020-04-23 20:55:14 +0300

    5.7 blogilistan frontend, step7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, {useState} from 'react'
+
+const Blog = ({ blog }) => {
+  const [showAll, setShowAll] = useState(false)
+  
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if (showAll) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} <button onClick={() => setShowAll(!showAll)}>hide</button><br/>
+        {blog.url}<br/>
+        likes {blog.likes} <button>like</button><br/>
+        {blog.author}
+      </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={() => setShowAll(!showAll)}>view</button>
+    </div>
+  )
+}
 
 export default Blog
